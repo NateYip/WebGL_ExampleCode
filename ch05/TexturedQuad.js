0b37d01804d76725d0141fd6ea18b2e00012e3b0
nateyip@outlook.com <nateyip@outlook.com>
2022-03-16 02:02:55 +0800

    feat(texturequad): 新的textParammeteri

--- ch05/TexturedQuad.js
+++ ch05/TexturedQuad.js
@@ -135,6 +135,8 @@ function loadTexture(gl, n, texture, u_Sampler, image) {
 
   // Set the texture parameters
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.MIRRORED_REPEAT);
   // Set the texture image
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
   
